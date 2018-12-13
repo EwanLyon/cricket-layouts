9ac52aed8d4fe9fa4c36b6e4dc681b9f8b374ee7
Clubwho <clubwhomc@gmail.com>
2018-12-13 18:50:24 +1100

    Begin listing players

--- dashboard/elements/cricket-fielders/cricket-fielders.js
+++ dashboard/elements/cricket-fielders/cricket-fielders.js
@@ -1,12 +1,21 @@
 import * as tslib_1 from "tslib";
-const { customElement } = Polymer.decorators;
+const { customElement, property } = Polymer.decorators;
 let CricketFielders = class CricketFielders extends Polymer.Element {
     ready() {
         super.ready();
     }
 };
+tslib_1.__decorate([
+    property({ type: String })
+], CricketFielders.prototype, "name", void 0);
+tslib_1.__decorate([
+    property({ type: String })
+], CricketFielders.prototype, "role", void 0);
+tslib_1.__decorate([
+    property({ type: String })
+], CricketFielders.prototype, "bowlingStyle", void 0);
 CricketFielders = tslib_1.__decorate([
     customElement('cricket-fielders')
 ], CricketFielders);
 export default CricketFielders;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY3JpY2tldC1maWVsZGVycy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImNyaWNrZXQtZmllbGRlcnMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLE1BQU0sRUFBQyxhQUFhLEVBQUMsR0FBRyxPQUFPLENBQUMsVUFBVSxDQUFDO0FBRzNDLElBQXFCLGVBQWUsR0FBcEMsTUFBcUIsZUFBZ0IsU0FBUSxPQUFPLENBQUMsT0FBTztJQUV4RCxLQUFLO1FBQ1AsS0FBSyxDQUFDLEtBQUssRUFBRSxDQUFDO0lBQ2YsQ0FBQztDQUNELENBQUE7QUFMb0IsZUFBZTtJQURuQyxhQUFhLENBQUMsa0JBQWtCLENBQUM7R0FDYixlQUFlLENBS25DO2VBTG9CLGVBQWUifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY3JpY2tldC1maWVsZGVycy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImNyaWNrZXQtZmllbGRlcnMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLE1BQU0sRUFBQyxhQUFhLEVBQUUsUUFBUSxFQUFDLEdBQUcsT0FBTyxDQUFDLFVBQVUsQ0FBQztBQUdyRCxJQUFxQixlQUFlLEdBQXBDLE1BQXFCLGVBQWdCLFNBQVEsT0FBTyxDQUFDLE9BQU87SUFVeEQsS0FBSztRQUNQLEtBQUssQ0FBQyxLQUFLLEVBQUUsQ0FBQztJQUNmLENBQUM7Q0FDRCxDQUFBO0FBWEE7SUFEQyxRQUFRLENBQUMsRUFBQyxJQUFJLEVBQUUsTUFBTSxFQUFDLENBQUM7NkNBQ1o7QUFHYjtJQURDLFFBQVEsQ0FBQyxFQUFDLElBQUksRUFBRSxNQUFNLEVBQUMsQ0FBQzs2Q0FDWjtBQUdiO0lBREMsUUFBUSxDQUFDLEVBQUMsSUFBSSxFQUFFLE1BQU0sRUFBQyxDQUFDO3FEQUNKO0FBUkQsZUFBZTtJQURuQyxhQUFhLENBQUMsa0JBQWtCLENBQUM7R0FDYixlQUFlLENBYW5DO2VBYm9CLGVBQWUifQ==
--- dashboard/elements/cricket-fielders/cricket-fielders.ts
+++ dashboard/elements/cricket-fielders/cricket-fielders.ts
@@ -1,7 +1,15 @@
-const {customElement} = Polymer.decorators;
+const {customElement, property} = Polymer.decorators;
 
 @customElement('cricket-fielders')
 export default class CricketFielders extends Polymer.Element {
+	@property({type: String})
+	name: string;
+
+	@property({type: String})
+	role: string;
+
+	@property({type: String})
+	bowlingStyle: string;
 
     ready() {
 		super.ready();
