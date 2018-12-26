1e7ad0610d3a2415f783a63dbdb804c555ea9873
Clubwho <clubwhomc@gmail.com>
2018-12-27 01:53:56 +1100

    Simplify property from everything to just batters

--- dashboard/elements/cricket-batters/cricket-batters.ts
+++ dashboard/elements/cricket-batters/cricket-batters.ts
@@ -3,17 +3,19 @@ const {customElement, property} = Polymer.decorators;
 import {CurrentInnings} from '../../../src/types/schemas/currentInnings';
 const currentInningsRep = nodecg.Replicant<CurrentInnings>('currentInnings');
 
+import {Batter} from '../../../src/types/schemas/batter';
+
 @customElement('cricket-batters')
 export default class CricketBatters extends Polymer.MutableData(Polymer.Element) {
 	@property({type: Object})
-	currentInnings: CurrentInnings
+	currentBatters: Batter[];
 
     ready() {
 		super.ready();
 
 		currentInningsRep.on('change', newVal => {
-			this.currentInnings = newVal;
-			this.notifyPath('currentInnings');
+			this.currentBatters = newVal.batters;
+			this.notifyPath('currentBatters');
 		});
 	}
 }
