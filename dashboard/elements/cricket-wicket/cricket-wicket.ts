c62fa95c2992cd5f0a5050cb1dc3b55f66c80b0f
Clubwho <clubwhomc@gmail.com>
2018-12-27 01:45:43 +1100

    Wicket shows batsmen names

--- dashboard/elements/cricket-wicket/cricket-wicket.ts
+++ dashboard/elements/cricket-wicket/cricket-wicket.ts
@@ -1,5 +1,8 @@
 const {customElement, property} = Polymer.decorators;
 
+import {CurrentInnings} from '../../../src/types/schemas/currentInnings'
+const currentInningsRep = nodecg.Replicant<CurrentInnings>('currentInnings');
+
 @customElement('cricket-wicket')
 export default class CricketWicket extends Polymer.Element {
     @property({type: String})
@@ -7,4 +10,13 @@ export default class CricketWicket extends Polymer.Element {
 
     @property({type: String})
     batter2: string = "Batter 2";
+
+    ready() {
+        super.ready();
+
+        currentInningsRep.on('change', newVal => {
+            this.batter1 = newVal.battersFacing[0].name;
+            this.batter2 = newVal.battersFacing[1].name;
+        });
+    }
 }
